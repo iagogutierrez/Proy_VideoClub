fd3b63e5a22f1d6127461c56040ca1f4599db065
Javi <javialvarez450@gmail.com>
2021-11-10 10:16:53 +0100

    clase peli

--- SafaPelis/SafaVideo/models.py
+++ SafaPelis/SafaVideo/models.py
@@ -2,4 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class pelicula(models.Model):
+    idPeli = models.AutoField(primary_key = True)
+    titulo = models.CharField('Nombre de la Pelicula', max_length = 50, null = False, blank = False)
+    categoria = models.CharField('Enfoque de la Pelicula', max_length = 30 ,null = False, blank = False)
+    fech_alquiler = models.DateField('Fecha Alquiler', null = False, blank = False)
+    fech_devol = models.DateField('Fecha Devolucion', null = False, blank = False)
 
+    class Meta:
+        verbose_name = 'Película'
+        verbose_name_plural = 'Películas'
